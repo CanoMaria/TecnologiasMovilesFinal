6b7fde6e3e21fd6125597388bb93cdefb3efab97
Mauricio Elgueta <maur.elgueta@gmail.com>
2022-08-09 16:09:48 -0300

    first commit

---
+++ app/src/main/java/com/example/canomariaayelenfinal/ui/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.canomariaayelenfinal.ui;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.canomariaayelenfinal.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin= findViewById(R.id.loginButton);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+                Toast.makeText(getApplicationContext(),"Bienvenido",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
