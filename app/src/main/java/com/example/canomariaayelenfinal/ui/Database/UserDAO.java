199d402083fedd8eed8ac93e985c53bb91d95262
CanoMaria <ayecano98@gmail.com>
2023-01-31 17:37:54 -0300

    added user db, register and login

---
+++ app/src/main/java/com/example/canomariaayelenfinal/ui/Database/UserDAO.java
@@ -0,0 +1,68 @@
+package com.example.canomariaayelenfinal.ui.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.canomariaayelenfinal.ui.Users.Users;
+
+import java.util.ArrayList;
+
+public class UserDAO {
+    Context context;
+    Users user;
+    ArrayList<Users> usersList;
+    SQLiteDatabase sql;
+    String db = "UserDB";
+
+
+    public UserDAO(Context context) {
+        this.context = context;
+        //comando para abrir o crear bd
+        sql= context.openOrCreateDatabase(db,context.MODE_PRIVATE,null);
+        sql.execSQL("create table if not exists users (id integer primary key autoincrement,email text,username text, password text, name text, surname text)");
+        user=new Users();
+    }
+
+    public boolean insertUser(Users user){
+        if(userAlreadyExist(user.getUsuario())==false){
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("email",user.getEmail());
+            contentValues.put("username",user.getUsuario());
+            contentValues.put("password",user.getPassword());
+            contentValues.put("name",user.getNombre());
+            contentValues.put("surname",user.getApellido());
+            return (sql.insert("users",null,contentValues)>0);
+        }else{
+            return false;
+        }
+    }
+
+    //Funcion que devuelve true si el usuario ya existe en la BD
+    private boolean userAlreadyExist(String username) {
+        Cursor cursor = sql.rawQuery("SELECT * FROM users WHERE username = ?", new String[] {username});
+        if (cursor.moveToFirst()) {
+            // El nombre de usuario se encuentra en la base de datos
+            cursor.close();
+            return true;
+        } else {
+            // El nombre de usuario no se encuentra en la base de datos
+            cursor.close();
+            return false;
+        }
+    }
+    public boolean userAndPasswordExist(String username,String password) {
+        Cursor cursor = sql.rawQuery("SELECT * FROM users WHERE username = ? AND password = ?", new String[] {username,password});
+        if (cursor.moveToFirst()) {
+            // El nombre de usuario se encuentra en la base de datos
+            cursor.close();
+            return true;
+        } else {
+            // El nombre de usuario no se encuentra en la base de datos
+            cursor.close();
+            return false;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/canomariaayelenfinal/ui/Users/Users.java
@@ -0,0 +1,104 @@
+package com.example.canomariaayelenfinal.ui.Users;
+
+import java.util.ArrayList;
+
+public class Users {
+    int id;
+    String nombre;
+    String apellido;
+    String usuario;
+    String password;
+
+    String email;
+
+    public Users() {
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Users(String nombre, String apellido, String usuario, String password, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.password = password;
+        this.email = email;
+    }
+
+    public void isAllNull(String nombre, String apellido, String usuario, String password, String email) throws Exception {
+        if (nombre.isEmpty() && apellido.isEmpty() && usuario.isEmpty() && password.isEmpty() && email.isEmpty()) {
+            throw new Exception("Todos los campos deben estar llenos");
+        }
+    }
+
+    public ArrayList<String> validateFields( String nombre, String apellido, String usuario, String password,String email) {
+        String[] fields = { "nombre", "apellido", "usuario", "password","email"};
+        String[] values = {nombre, apellido, usuario, password,email};
+        ArrayList<String> returnList = new ArrayList<String>();
+        for (int i = 0; i < fields.length; i++) {
+            if (values[i] == null || values[i].trim().length() == 0) {
+                returnList.add(fields[i]);
+            }
+        }
+        return returnList;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
